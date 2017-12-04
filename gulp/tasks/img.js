dc253b02deaa2da6b105b14884f091e4f7bd668d
OrionPro <orion79@rambler.ru>
2017-12-04 16:12:48 +0200

    first commit

---
+++ gulp/tasks/img.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+    $.gulp.task('img:dev', () => {
+        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+            .pipe($.gulp.dest('./build/static/img/'));
+    });
+
+    $.gulp.task('img:build', () => {
+        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+            .pipe($.gp.tinypng(YOUR_API_KEY))
+            .pipe($.gulp.dest('./build/static/img/'));
+    });
+
+
+    $.gulp.task('svg:copy', () => {
+        return $.gulp.src('./dev/static/img/general/*.svg')
+            .pipe($.gulp.dest('./build/static/img/general/'));
+    });
+};
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+global.$ = {
+    path: {
+        task: require('./gulp/paths/tasks.js')
+    },
+    gulp: require('gulp'),
+    del: require('del'),
+    fs: require('fs'),
+    browserSync: require('browser-sync').create(),
+    gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+    require(taskPath)();
+});
+
+$.gulp.task('dev', $.gulp.series(
+    'clean',
+    $.gulp.parallel('styles:dev', 'pug', 'libsJS:dev', 'js:copy', 'svg', 'img:dev', 'fonts','svg:copy')));
+
+$.gulp.task('build', $.gulp.series(
+    'clean',
+    $.gulp.parallel('styles:build', 'pug', 'libsJS:build', 'js:copy', 'svg', 'img:build', 'fonts','svg:copy')));
+
+$.gulp.task('default', $.gulp.series(
+    'dev',
+    $.gulp.parallel(
+        'watch',
+        'serve'
+    )
+));
